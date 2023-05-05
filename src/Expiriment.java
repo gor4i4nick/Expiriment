066a0f1fef18ac7095584391c78d9c1f77c912ed
Mykola <klopina@i.ua>
2023-05-05 06:42:11 +0300

    mY fIRST commit for expiriment

---
+++ src/Expiriment.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Expiriment extends JFrame {
+	private JTextField textField;
+	public Expiriment() {
+		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		textField = new JTextField();
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		getContentPane().add(btnNewButton);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
